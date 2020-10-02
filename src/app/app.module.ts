ed8d4fff4e391d29ec307c15ea7ab6355dacf3ce
AdamSwieciak <adamswiec@gmail.com>
2020-10-02 10:46:23 +0200

    routing works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component'
 import { DropdownDrirectives } from './shared/dropdown.directives'
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 @NgModule({
@@ -25,11 +28,14 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDrirectives
+    DropdownDrirectives,
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -14,7 +14,7 @@ export class RecipeService {
       new Ingredient('Meat', 1),
       new Ingredient ('salad', 3)
     ] ),
-    new Recipe('PAsta', 'this is', 'https://hips.hearstapps.com/delish/assets/17/36/1504715566-delish-fettuccine-alfredo.jpg', [
+    new Recipe('Pasta', 'this is', 'https://hips.hearstapps.com/delish/assets/17/36/1504715566-delish-fettuccine-alfredo.jpg', [
       new Ingredient('Ham', 5),
       new Ingredient('Chees', 1) 
     ] )
@@ -24,6 +24,10 @@ export class RecipeService {
 
   getRecipes() {
     return this.recipes.slice()
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index]
   }
 
   adNewIngredients(ingredients: Ingredient[]) {
